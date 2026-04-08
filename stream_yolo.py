463c6b042de1b936b6076a419fe5b3e3ba1405a9
oybeep <sys041001@gmail.com>
2026-04-08 18:25:43 +0900

    Update stream_yolo.py

--- stream_yolo.py
+++ stream_yolo.py
@@ -3,45 +3,32 @@ from ultralytics import YOLO
 from flask import Flask, Response
 
 app = Flask(__name__)
-
 model = YOLO('/home/team-d/obstacle_detection/best_incomplete_ncnn_model', task='detect')
 
 def generate_frames():
     cap = cv2.VideoCapture(0)
-
-    count = 0 
-    annotated_frame = None
-
+    
     while True:
         success, frame = cap.read()
-        if not success:
-            break
+        if not success: break
+
+        results = model.predict(
+            source=frame, 
+            imgsz=320, 
+            conf=0.5,      # 신뢰도 50%
+            iou=0.45,       # 이 값을 조절하면 겹치는 박스가 사라짐
+            verbose=False,
+            stream=False
+        )
+
+        annotated_frame = results[0].plot() 
         
-        count += 1
-
-        if count % 3 == 0:
-            results = model.predict(frame, imgsz=160, conf=0.5, verbose=False)
-
-            annotated_frame = results[0].plot(line_width=2, labels=True, boxes=True)
-
-        if annotated_frame is None:
-            display_frame = frame
-        else:
-            display_frame = annotated_frame
-
-        ret, buffer = cv2.imencode('.jpg', display_frame)
-        frame_bytes = buffer.tobytes()
-        
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
 
 @app.route('/')
-def index():
-    return '<body style="margin:0; background:#000;"><img src="/video_feed" style="width:100%; height:auto;"></body>'
-
-@app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000)
